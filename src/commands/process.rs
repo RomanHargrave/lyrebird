71f10ce6a38814e53acfaebb6c2349b223427568
Roman Hargrave <roman@hargrave.info>
2022-06-18 16:02:38 -0700

    doc: add notes to exec subcommand implementation

--- src/commands/process.rs
+++ src/commands/process.rs
@@ -16,15 +16,23 @@ pub struct StartProcessArgs {
   args: Vec<String>
 }
 
+/// Start a process based on command line parameters and record having
+/// done so in the log.
 pub fn start_process(log: &mut Log, params: &StartProcessArgs) -> OrErrorBox {
+  // Create a new command builder with the given executable name and
+  // arguments.
   Command::new(&params.exe)
     .args(&params.args)
+    // start the command
     .spawn()
     .map_err(|e| e.into())
+    // If Command succeeded, extract what we need for the log from the
+    // Child structure
     .map(|child| json!({
       "cmd":  &params.exe,
       "args": &params.args,
       "pid":  child.id()
     }))
+    // And then place the extracted data in the log
     .and_then(|rec| log.record_action("StartProcess", &rec))
 }
