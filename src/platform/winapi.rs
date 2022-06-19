5b766146bf6f010849357be21787a81b11c336d6
Roman Hargrave <roman@hargrave.info>
2022-06-18 21:51:48 -0700

    feat: windows username discovery
    i did not enjoy this

--- src/platform/winapi.rs
+++ src/platform/winapi.rs
@@ -1,25 +1,39 @@
 /* Platform Support Abstractions (Windows) - (C) 2022 Roman Hargrave <roman@hargrave.info>
  */
 
-use winapi::{
-  DWORD,
-  GetUserNameA
-}
-
 use std::ptr;
+use std::string::String;
 
-pub const LOG_FILE = "%TMP%/lyrebird.log";
+use windows::core::PWSTR;
+use windows::Win32::System::WindowsProgramming::GetUserNameW;
+
+pub const LOG_FILE: &str = "lyrebird.log";
 
 pub fn get_username() -> Option<String> {
   /* In this case, WINAPI is just slightly more tolerable than POSIX,
    * since we can actually poke it for the expected size of the
    * username and allocate just that.
    */
-  let mut name_length: DWORD = 0;
-  let mut name_buf
+  let mut name_length: u32 = 0;
+  let mut name_buf: Vec<u16> = vec![];
 
   // first, we need to figure out how many bytes we actually need.
-  unsafe {
-    GetUserNameA(ptr::null_mut(), &mut name_length);
-  }
+  // according to windows docs, this should populate name length with 
+  // some value > 0 and return FALSE
+  unsafe { GetUserNameW(PWSTR(ptr::null_mut()), &mut name_length) };
+
+  assert!(name_length > 0, "GetUserNameW set pcbBuffer <= 0");
+
+  // now, we should have a username length (in 16-bit codepoints)
+  name_buf.resize(name_length.try_into().unwrap(), 0);
+
+  // GetUserNameW should now return TRUE
+  unsafe { GetUserNameW(PWSTR(name_buf.as_mut_ptr()), &mut name_length) }
+    .expect("GetUserNameW failed");
+
+  // Remove trailing null character
+  let mut name = String::from_utf16_lossy(&name_buf);
+  name.truncate(name.len() - 1);
+
+  Some(name)
 }
